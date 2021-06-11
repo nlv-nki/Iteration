f43563d8fc68ed5528795cece851631bd50283ce
Nikolai <nlv.nki@gmail.com>
2021-06-11 16:23:20 +0300

    ex. 7 complite

--- Exercises/7-ages.js
+++ Exercises/7-ages.js
@@ -1,4 +1,10 @@
 'use strict';
+const persons = {
+  lenin: { born: 1870, died: 1924 },
+  mao: { born: 1893, died: 1976 },
+  gandhi: { born: 1869, died: 1948 },
+  hirohito: { born: 1901, died: 1989 },
+};
 
 const ages = persons => {
   // Use for..in to calculate age for each person
@@ -14,6 +20,19 @@ const ages = persons => {
   //   gandhi: 79,
   //   hirohito: 88,
   // }
+
+  let result = {}
+  // for (let person in persons) {
+  //   console.log(person);
+  //   result[person] = persons[person].died - persons[person].born;
+  // }
+
+ result = Object.keys(persons)
+ .forEach(person => result[person] = persons[person].died - persons[person].born);
+ return result;
+
 };
+console.log(ages(persons));
+
 
 module.exports = { ages };
